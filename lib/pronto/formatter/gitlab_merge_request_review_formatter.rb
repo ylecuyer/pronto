272680858d565057a26b2da8f495d4898529f535
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2021-03-26 11:57:13 +0100

    some debug

--- lib/pronto/formatter/gitlab_merge_request_review_formatter.rb
+++ lib/pronto/formatter/gitlab_merge_request_review_formatter.rb
@@ -10,15 +10,15 @@ module Pronto
       end
 
       def existing_comments(_, client, repo)
+        puts "existing"
         sha = repo.head_commit_sha
         comments = client.pull_comments(sha)
         grouped_comments(comments)
       end
 
       def submit_comments(client, comments)
+        puts "submit"
         client.create_pull_request_review(comments)
-      rescue => e
-        $stderr.puts "Failed to post: #{e.message}"
       end
 
       def line_number(message, _)
